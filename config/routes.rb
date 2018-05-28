9258bd0afac639006be3b5be2c8696e79ccace8a
Ryan Salomon <ryan5231@gmail.com>
2018-05-28 14:39:08 -0700

    tests and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get '/about' => 'static_pages#about'
+get 'static_pages/home'
+get 'static_pages/about'
 root 'static_pages#home'
 
 
