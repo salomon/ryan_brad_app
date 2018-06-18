7fe10b7d723af3850954ac6a33e8ec4de19e0086
Ryan Salomon <ryan5231@gmail.com>
2018-06-18 16:38:44 -0700

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 
 module ApplicationHelper
 
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Buddies App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ end
 	test "should get home" do
 		get root_url
 		assert_response :success
-		assert_select "title", "Home | #{@base_title}"
+		assert_select "title", "#{@base_title}"
 	end
 
 	test "should get about" do
@@ -20,8 +20,8 @@ end
 	end
 
 	test "should get root" do
-    get root_url
-    assert_response :success
+	    get root_url
+	    assert_response :success
     end
 
 
