37ce8ff9d945b6bd9684b85bf91361244f22af7b
Ryan Salomon <ryan5231@gmail.com>
2018-07-18 13:00:05 -0700

    Implemented basic login and logout

--- Gemfile
+++ Gemfile
@@ -43,8 +43,8 @@ group :test do
 end
 
 group :production do
-  gem 'pg',  '0.20.0'
-  gem 'fog', '1.42'
+  gem 'pg',   '0.20.0'
+  gem 'fog',  '1.42'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Buddies App!"
       redirect_to @user
     else
       render 'new'
